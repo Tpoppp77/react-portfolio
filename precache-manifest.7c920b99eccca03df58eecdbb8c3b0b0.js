83df5a6b897cfbdc8f024450f7a9aa8d22bc9ae9
Tpoppp <olson0002@yahoo.com>
2020-07-28 17:44:26 -0700

    Updates

---
+++ precache-manifest.7c920b99eccca03df58eecdbb8c3b0b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f295b034ae6c75f6e77151b56fbe308",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c776a87e99b81355295c",
+    "url": "/react-portfolio/static/css/main.a4c79ee2.chunk.css"
+  },
+  {
+    "revision": "746bef788416a66c2b11",
+    "url": "/react-portfolio/static/js/2.b71896c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.b71896c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c776a87e99b81355295c",
+    "url": "/react-portfolio/static/js/main.c4fd9b35.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
